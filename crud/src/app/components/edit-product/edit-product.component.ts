55f21a28b651eee6a5566643a46bb4c5f02b0a4b
Juan Luis <66567588+titan54321@users.noreply.github.com>
2025-11-14 00:43:09 +0000

    Creacion de Componentes

---
+++ crud/src/app/components/edit-product/edit-product.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-product',
+  imports: [],
+  templateUrl: './edit-product.component.html',
+  styleUrl: './edit-product.component.css'
+})
+export class EditProductComponent {
+
+}
---
+++ crud/src/app/components/form-product/form-product.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form-product',
+  imports: [],
+  templateUrl: './form-product.component.html',
+  styleUrl: './form-product.component.css'
+})
+export class FormProductComponent {
+
+}
